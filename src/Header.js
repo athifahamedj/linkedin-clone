ba5f4b90e8fafe474de7ca6ce611fd03c8f10f00
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-11-30 12:04:13 +0530

    files updates

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+function Header() {
+return (
+<React.Fragment> 
+  <div className='header'>
+   <h2>Header File</h2>
+    <div className="header__left">
+
+      <img src= "" alt="no image"/>
+
+      <div className="header__search">
+
+        <SearchIcon/>
+
+      </div>
+
+    </div>
+    <div className="header__right">
+
+    </div>
+   
+   </div>
+   
+   </React.Fragment>
+  
+
+)
+
+}
+
+export default Header;
