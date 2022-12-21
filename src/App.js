5c51968690d4a7292c3a1c0cdc621f95403d2878
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-12-21 18:38:46 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-function App() {
+function App() {      
   return (
     <div className="app">
        <Header />
