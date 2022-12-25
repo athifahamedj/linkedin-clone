06a6cca4055e50efe5251562cd7d48bf479b78ca
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-12-25 10:31:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
        <Header />
-      <div className="app__body">
+      <div className="app__body"> 
         <Sidebar/>
        {/* feed */}
        {/* widgets */}
